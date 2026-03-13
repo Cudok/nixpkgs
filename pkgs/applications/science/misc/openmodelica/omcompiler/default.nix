f4d579660d46a35b78359e0224a9491723ee462c
Falk Cudok <falkcudok@web.de>
2026-03-13 09:36:13 +0100

    remove not used lines (until now)

--- pkgs/applications/science/misc/openmodelica/omcompiler/default.nix
+++ pkgs/applications/science/misc/openmodelica/omcompiler/default.nix
@@ -14,17 +14,7 @@ mkOpenModelicaDerivation
     pname = "omcompiler";
     src = ./.;
 
-  # buildInputs = [
-  #   pkgs.ffmpeg
-  # ];
-  # # only for the build process
-  # nativeBuildInputs = [
-  #   pkgs.pkgs.config
-  # ];
-  # unpackPhase = '' # shell logic '';
   buildPhase = ''
     echo 'hello worllld' > $out
   '';
-  # installPhase = '' # shell logic '';
-  # ...
 }
