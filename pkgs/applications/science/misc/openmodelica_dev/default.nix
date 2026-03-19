687b14332c51c34de894a3b9056f40c74ff514a4
Falk Cudok <falkcudok@web.de>
2026-03-19 17:11:16 +0100

    remove unnecessary lines

--- pkgs/applications/science/misc/openmodelica_dev/default.nix
+++ pkgs/applications/science/misc/openmodelica_dev/default.nix
@@ -32,12 +32,6 @@ stdenv.mkDerivation rec {
       sha256 = "sha256-GuY8vMF2Hsr4KKtp73/VYqnex9Kwtc2O4KsfIXxOlSY=";
       fetchSubmodules = true;
     };
-  # src = fetchgit {
-  #   url = "https://github.com/OpenModelica/OpenModelica/";
-  #   rev = "f10cf343dc90e6ce1eb9a0b72b01c9b6398af5c1";
-  #   sha256 = "09lm9ry227xbw27cvddhsb3xxab2smzyysdb53wcl7knq6y3rrhs";
-  #   fetchSubmodules = true;
-  # };
 # Update with: nix run -f ./nixpkgs/default.nix nix-prefetch-git -c nix-prefetch-git 'https://github.com/OpenModelica/OpenModelica/' 'v1.18.0' --fetch-submodules
 #  nix-prefetch-git 'https://github.com/OpenModelica/OpenModelica/' 'v1.26.3' --fetch-submodules
 
